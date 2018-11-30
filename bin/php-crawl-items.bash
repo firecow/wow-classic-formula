1630db9cb0b7ee51b5f51f737e9792f6ae70e208
Mads Jon Nielsen <madsjon@gmail.com>
2018-11-30 14:21:19 +0100

    Don't create data folders.

--- bin/php-crawl-items.bash
+++ bin/php-crawl-items.bash
@@ -2,7 +2,6 @@
 docker run --rm -t \
     --volume "/${PWD}/php/php.ini:/usr/local/etc/php/conf.d/php.ini" \
     --volume "/${PWD}/php:/php" \
-    --volume "/${PWD}/data:/data" \
     --network wow-classic-formula_default \
     -w "//php/" \
     nanoninja/php-fpm:7.2 php crawl_items.php \
