48e5890f7c1f81503d692d69bf3cc054309fb07f
Mads Jon Nielsen <madsjon@gmail.com>
2018-11-30 07:44:45 +0100

    Added php easy coding standard

---
+++ bin/php-ecs-fix.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker run --rm -t \
+    --volume "/${PWD}/php/easy-coding-standard.yml:/app/easy-coding-standard.yml" \
+    --volume "/${PWD}/php/src:/app/src" \
+    --volume "/${PWD}/php/routes:/app/routes" \
+    --volume "/${PWD}/php/vendor:/app/vendor" \
+    -w "//app/" \
+    php:7.2-cli \
+    ./vendor/bin/ecs check --fix src/ routes/ \
