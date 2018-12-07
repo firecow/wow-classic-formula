a8d1c7f1714b53c863d28e62c7a2034060213ea9
Mads Jon Nielsen <madsjon@gmail.com>
2018-12-07 13:48:35 +0100

    Added refresh_locations scripts

--- bin/php-refresh-locations.bash
+++ bin/php-refresh-locations.bash
@@ -3,6 +3,7 @@ docker run --rm -t \
     --volume "/${PWD}/php/php.ini:/usr/local/etc/php/conf.d/php.ini" \
     --volume "/${PWD}/php:/php" \
     --volume "/${PWD}/data:/data" \
+    --volume "/${PWD}/dumps:/dumps" \
     --network wow-classic-formula_default \
     -w "//php/" \
     nanoninja/php-fpm:7.2 php refresh_locations.php \
--- php/refresh_locations.php
+++ php/refresh_locations.php
@@ -16,6 +16,66 @@ $sql = new SQL($config->getPDODataSourceName(), $config->getPDOUsername(), $conf
 
 $json = JSON::decode(file_get_contents("../dumps/instances.json"));
 
-foreach ($json as $list) {
+$instances = [
+    "Mara",
+    "ST",
+    "BRD",
+    "DME",
+    "DMN",
+    "DMW",
+    "UBRS",
+    "STRAT",
+    "LBRS",
+    "SCHOLO",
+];
 
+$raids = [
+    "BWL",
+    "MC",
+    "Onyxia",
+    "ZG",
+    "AQ20",
+    "AQ40",
+    "NAX",
+];
+
+$raidRegExp = "/^(";
+$raidRegExp .= implode("|", $raids);
+$raidRegExp .= ")(.*)/";
+
+$instanceRegExp = "/^(";
+$instanceRegExp .= implode("|", $instances);
+$instanceRegExp .= ")(.*)/";
+
+$sql->execute("TRUNCATE item_locations", []);
+
+
+foreach ($json as $atlasKey => $list) {
+    if (preg_match($instanceRegExp, $atlasKey, $matches)) {
+        $instanceName = $matches[1];
+        $bossName = $matches[2];
+
+        foreach ($list as $itemData) {
+            $itemId = $itemData[0];
+            if ($itemId != 0) {
+                $itemName = preg_replace("/=>.*=>/", "", $itemData[2]);
+                $sql->execute("REPLACE INTO item_locations VALUES (?, ?, ?, ?, ?)", [$itemId, $itemName, $instanceName, $bossName, 0]);
+            }
+        }
+
+    } else if (preg_match($raidRegExp, $atlasKey, $matches)) {
+        $raidName = $matches[1];
+        $bossName = $matches[2];
+
+        foreach ($list as $itemData) {
+            $itemId = $itemData[0];
+            if ($itemId != 0) {
+                $itemName = preg_replace("/=>.*=>/", "", $itemData[2]);
+                $sql->execute("REPLACE INTO item_locations VALUES (?, ?, ?, ?, ?)", [$itemId, $itemName, $raidName, $bossName, 1]);
+            }
+        }
+
+    } else {
+        echo "Skipping: $atlasKey\n";
+    }
 }
