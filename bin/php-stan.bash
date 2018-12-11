373329e12e92239eec47294d4621b54325e48ffd
Mads Jon Nielsen <madsjon@gmail.com>
2018-12-11 14:21:31 +0100

    Simplified volumes

--- bin/php-stan.bash
+++ bin/php-stan.bash
@@ -1,9 +1,6 @@
 #!/bin/bash
 docker run --rm -t \
-    --volume "/${PWD}/php/src:/app/src:ro" \
-    --volume "/${PWD}/php/cli:/app/cli:ro" \
-    --volume "/${PWD}/php/routes:/app/routes:ro" \
-    --volume "/${PWD}/php/vendor:/app/vendor:ro" \
+    --volume "/${PWD}/php/:/app/" \
     -w "//app/" \
     php:7.2-cli \
     bash -c "./vendor/bin/phpstan analyse --no-progress -c vendor/phpstan/phpstan-strict-rules/rules.neon -l max src/ routes/ cli/" \
