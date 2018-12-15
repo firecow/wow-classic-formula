900847a8a4c46206560aa09e439e02c343d3b59f
Mads Jon Nielsen <madsjon@gmail.com>
2018-12-15 21:07:10 +0100

    Made convert script great again.

--- php/cli/atlasloot_convert.php
+++ php/cli/atlasloot_convert.php
@@ -13,27 +13,39 @@ require '/php/error.php';
 $config = new Config();
 $sql = new SQL($config->getPDODataSourceName(), $config->getPDOUsername(), $config->getPDOPassword());
 
-$files = ["/dumps/crafting.en.lua"];
+$files = ["/dumps/Battlegrounds.en.lua"];
 
 echo "Converting\n";
 
 foreach ($files as $fileName) {
     $contents = file_get_contents($fileName);
 
+    $contents = preg_replace("/^\]\,$/m", ']', $contents);
     $contents = preg_replace("/--.*--/", "", $contents);
+    $contents = preg_replace("/--[\S]*/", "", $contents);
     $contents = preg_replace("/[\t ]*(.*) = {/", "'$1' => {", $contents);
-    $contents = preg_replace("/'AtlasLoot_Data[\"AtlasLootCrafting\"]' =>/", '$map =', $contents);
     $contents = preg_replace("/{/", "[", $contents);
     $contents = preg_replace("/}/", "]", $contents);
     $contents = preg_replace("/];/", "],", $contents);
+    $contents = preg_replace("/'AtlasLoot_Data\[\"AtlasLootCrafting\"\]' => \[/", '$map = [', $contents);
+    $contents = preg_replace("/'AtlasLoot_Data\[\"AtlasLootSetItems\"\]' => \[/", '$map = [', $contents);
 
 
     $php = "<?php\n";
     $php .= $contents . ";";
 
     $phpFileName = str_replace(".lua", ".php", $fileName);
-    echo "$phpFileName\n";
     file_put_contents($phpFileName, $php);
+
+    require $phpFileName;
+
+    $jsonFileName = str_replace(".lua", ".json", $fileName);
+    echo "$phpFileName\n";
+    file_put_contents($jsonFileName, json_encode($map, JSON_PRETTY_PRINT));
 }
+
+
+
+
 
 echo "Success\n";
