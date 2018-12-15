5745d2366dcdd675c5618813c0287c8f56c5dcf0
Mads Jon Nielsen <madsjon@gmail.com>
2018-12-15 21:08:29 +0100

    Correct userid

--- bin/php-atlasloot-convert.sh
+++ bin/php-atlasloot-convert.sh
@@ -4,5 +4,6 @@ docker run --rm -t \
     --volume "/${PWD}/php:/php" \
     --volume "/${PWD}/dumps:/dumps" \
     --network wow-classic-formula_default \
+    --user $(id -u):$(id -g) \
     -w "//php/" \
     nanoninja/php-fpm:7.2 php cli/atlasloot_convert.php \
