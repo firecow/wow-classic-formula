ceda534da7eef5f03b934f365449b8bf5ff4f155
Mads Jon Nielsen <madsjon@gmail.com>
2018-12-15 22:17:26 +0100

    Add correct userid/groupid

--- bin/php-sandbox.sh
+++ bin/php-sandbox.sh
@@ -3,6 +3,7 @@ docker run --rm -t \
     --volume "/${PWD}/php/php.ini:/usr/local/etc/php/conf.d/php.ini" \
     --volume "/${PWD}/php:/php" \
     --volume "/${PWD}/dumps:/dumps" \
+    --user $(id -u):$(id -g) \
     --network wow-classic-formula_default \
     -w "//php/" \
     nanoninja/php-fpm:7.2 php cli/sandbox.php \
